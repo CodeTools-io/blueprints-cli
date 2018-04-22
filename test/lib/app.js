72a7ad83378a4d4ece5ee4264e53761e8091f4eb
Cliff Pyles <cliff@cliffpyles.com>
2018-04-22 09:48:02 -0400

    Add initial test scenarios

--- test/lib/app.js
+++ test/lib/app.js
@@ -1,7 +1,11 @@
 const { expect } = require('chai')
 
 describe('App', function() {
-  it('should execute', function() {
-    expect(true).to.eql(true)
-  })
+  it.skip('can load global config')
+  it.skip('can load project config')
+  it.skip('can merge project config into global config')
+  it.skip('can generate global blueprints')
+  it.skip('can generate from project blueprints')
+  it.skip('can replace blueprint template variables')
+  it.skip('can rename files and directories')
 })
