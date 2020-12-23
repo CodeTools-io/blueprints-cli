f4458da932ab29cbbb0f7176c2e50552a6fbd188
Cliff Pyles <cliff@cliffpyles.com>
2020-12-23 07:33:40 -0500

    Update default required modules in the action blueprint

--- .blueprints/action/files/app/actions/__blueprintInstance__/__blueprintInstance__.js
+++ .blueprints/action/files/app/actions/__blueprintInstance__/__blueprintInstance__.js
@@ -1,3 +1,13 @@
+const path = require('path')
+const fs = require('fs-extra')
+
+const Blueprint = require('../../lib/Blueprint')
+
+const {
+  PROJECT_BLUEPRINTS_PATH,
+  GLOBAL_BLUEPRINTS_PATH,
+} = require('../../config')
+
 module.exports = async function {{blueprintInstance}}(options) {
   try {
     // action steps
