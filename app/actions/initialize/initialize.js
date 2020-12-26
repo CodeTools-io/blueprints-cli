4c2dd6f76625552b8c13d4c3fd94ca173e813d8a
Cliff Pyles <cliff@cliffpyles.com>
2020-12-26 07:53:44 -0500

    Remove unused config from initialize action

--- app/actions/initialize/initialize.js
+++ app/actions/initialize/initialize.js
@@ -1,13 +1,6 @@
 const path = require('path')
 const fs = require('fs-extra')
 
-const {
-  CURRENT_DIRNAME,
-  CURRENT_PATH,
-  PROJECT_BLUEPRINTS_PATH,
-  GLOBAL_BLUEPRINTS_PATH,
-} = require('../../config')
-
 module.exports = async function initialize(projectPath, command) {
   const projectBlueprintsPath = projectPath
     ? path.resolve(projectPath, './.blueprints')
