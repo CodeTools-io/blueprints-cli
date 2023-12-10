7b5e2d507543e837e658005d3ec77d36c9cd7e54
Cliff Pyles <cliff@cliffpyles.com>
2023-12-10 07:11:05 -0500

    Remove irrelevant comments from utilities

--- app/utilities.js
+++ app/utilities.js
@@ -3,25 +3,6 @@ const globby = require('globby')
 const inflection = require('inflection')
 const prop = require('dot-prop')
 
-// Filename: ./app/utils/get-template-data.js
-// const setValue = require('./set-value')
-
-// function getTemplateData(args) {
-//   const rawData = args.filter((arg) => !arg.startsWith('--'))
-
-//   return rawData.reduce((data, arg) => {
-//     const [key, value] = arg.split('=', 2)
-
-//     return setValue(data, key, value)
-//   }, {})
-// }
-
-// module.exports = getTemplateData
-
-// Filename: ./app/utils/getAbsolutePaths/getAbsolutePaths.js
-// const path = require('path')
-// const globby = require('globby')
-
 function getAbsolutePaths(glob, options) {
   return globby(glob, options).then((results) => {
     const cwd = options.cwd ? options.cwd : process.cwd()
@@ -31,12 +12,6 @@ function getAbsolutePaths(glob, options) {
     })
   })
 }
-
-// Filename: ./app/utils/getAbsolutePaths/index.js
-// module.exports = require('./getAbsolutePaths')
-
-// Filename: ./app/utils/getMetadata/getMetadata.js
-// const inflection = require('inflection')
 
 function getMetadata({ blueprintInstance, blueprint }) {
   const standardBlueprintInstance = blueprintInstance.replace(/-/gi, '_')
@@ -101,14 +76,6 @@ function getMetadata({ blueprintInstance, blueprint }) {
   }
 }
 
-// module.exports = getMetadata
-
-// Filename: ./app/utils/getMetadata/index.js
-// module.exports = require('./getMetadata')
-
-// Filename: ./app/utils/getObject/getObject.js
-// const prop = require('dot-prop')
-
 function getObject(keyValueEntries) {
   let result = {}
   keyValueEntries.forEach(([key, value]) => {
@@ -138,42 +105,18 @@ function getObject(keyValueEntries) {
   return result
 }
 
-// Filename: ./app/utils/getObject/index.js
-// module.exports = require('./getObject')
-
-// Filename: ./app/utils/getParsedKeyValues/getParsedKeyValues.js
 function getParsedKeyValues(keyValues = []) {
   return keyValues.map((keyVal) => keyVal.split('=', 2))
 }
 
-// Filename: ./app/utils/getParsedKeyValues/index.js
-// module.exports = require('./getParsedKeyValues')
-
-// Filename: ./app/utils/getTemplateArgs/getTemplateArgs.js
 function getTemplateArgs(argv = []) {
   return argv.filter((arg) => !arg.startsWith('--'))
 }
-
-// Filename: ./app/utils/getTemplateArgs/index.js
-// module.exports = require('./getTemplateArgs')
-
-// Filename: ./app/utils/getTemplateData/getTemplateData.js
-// const pipe = require('../pipe')
-// const getParsedKeyValues = require('../getParsedKeyValues')
-// const getTemplateArgs = require('../getTemplateArgs')
-// const getObject = require('../getObject')
 
 function getTemplateData(args) {
   return pipe(args, getTemplateArgs, getParsedKeyValues, getObject)
 }
 
-// Filename: ./app/utils/getTemplateData/index.js
-// module.exports = require('./getTemplateData')
-
-// Filename: ./app/utils/log/index.js
-// module.exports = require('./log')
-
-// Filename: ./app/utils/log/log.js
 let queue = []
 
 function log(...value) {
@@ -212,24 +155,10 @@ log.clear = function () {
   queue = []
 }
 
-// module.exports = log
-
-// Filename: ./app/utils/pipe/index.js
-// module.exports = require('./pipe')
-
-// Filename: ./app/utils/pipe/pipe.js
 function pipe(value, ...fns) {
   return fns.reduce((accum, fn) => fn(accum), value)
 }
 
-// Filename: ./app/utils/set-value.js
-// const prop = require('dot-prop')
-
-// Sets a property on an object based on a string
-// Accepted Keys:
-// - propName
-// - propName.nestedProp
-// - propName.nestedProp[indexNumber]
 function setValue(data, key, value) {
   const arrayRegex = /([\w\.]+)\[(\d)*\]/
   const keySections = key.match(arrayRegex)
@@ -255,7 +184,6 @@ function setValue(data, key, value) {
   return data
 }
 
-// module.exports = setValue
 module.exports = {
   getTemplateData,
   getAbsolutePaths,
