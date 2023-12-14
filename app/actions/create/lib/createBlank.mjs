786cebf357caa9a4bf3e1e53fd438a929714c9d7
Cliff Pyles <cliff@cliffpyles.com>
2023-12-14 05:32:05 -0500

    Update default prompt for new blueprints

--- app/actions/create/lib/createBlank.mjs
+++ app/actions/create/lib/createBlank.mjs
@@ -18,10 +18,21 @@ export default async function(data, libraries) {
 `
 const DEFAULT_PROMPT = `
 ---
-input_variables: []
-partial_variables: []
+input_variables:
+  - template_variables
+  - templates
+  - format_instructions
 ---
-You are a file generator. You will generate files using the provided template and data.
+You are a file generator called a blueprint. You will generate files using the provided templates and template variables.
+
+{format_instructions}
+
+template_variables:
+{template_variables}
+
+templates:
+{templates}
+
 `
 
 export default async function createBlank(blueprintName, options = {}) {
